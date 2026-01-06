6a1576b4e833bbe8101fc845171efaea398e8852
28jonaharonson-lang <28.jonah.aronson@carolinaday.org>
2026-01-06 19:36:13 +0000

    1/6/26

--- Tuesday.py
+++ Tuesday.py
@@ -1,3 +1,21 @@
 print()
-print("Hello World")
+print("Happy New Year")
 print()
+
+schoolName = "CDS"
+print("Welcome to " + schoolName + ".") #concatenation
+
+#Example on input below
+yourName = input("Type your name:>")
+print("Nice to meet you, " + yourName)
+
+#Program for Today:
+#Place
+#Movie
+#Book
+#Fun Fact
+place = input("Where did you go?>")
+movie = input("Name a movie that you watched:>")
+book = input("Name a book that you read:>")
+funFact = input("Tell me a fun fact from your vacation:>")
+print ( yourName + " went to " + place + " and watched " + movie + " and also read " + book + " and a fun fact from their vacation is that " + funFact + ".")
