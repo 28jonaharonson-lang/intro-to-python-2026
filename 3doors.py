cfe7d1b57ee4105294ba1f64f87b2af87b214f36
28jonaharonson-lang <28.jonah.aronson@carolinaday.org>
2026-01-22 16:37:04 +0000

    3d

---
+++ 3doors.py
@@ -0,0 +1,21 @@
+import random
+
+
+wins = 0
+runs = 0
+for i in range(0,1000000000):
+     runs = runs + 1
+     print("Simulation #" + str(runs))
+     winningDoor = random.randint(1, 3)
+     contestChoice = random.randint(1, 3)
+     revealDoor = random.randint(1,3)
+     contestFinal = random.randint(1,3)
+     while revealDoor == winningDoor or revealDoor == contestChoice:
+        revealDoor = random.randint(1,3)
+     while contestFinal == contestChoice or contestFinal == revealDoor:
+              contestFinal = random.randint(1,3)
+     if contestFinal == winningDoor:
+          wins = wins + 1
+
+print(wins)
+print(runs)
